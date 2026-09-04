d5c4906b32732c3bc278a2002f4eff93bdf0e9a1
Zandi Galpin <zandi.galpin@live.nmit.ac.nz>
2026-09-05 00:30:42 +1200

    added function to build grid locations with direction exits and a separate locked door functionality. builds all grid locations, assigns N/e/s/w, assigns story text

--- game/map_data.py
+++ game/map_data.py
@@ -1,3 +1,15 @@
+from game.location import Location
+
+#set here in case map gets made bigger later.
+GRID_ROWS = 4
+GRID_COLUMNS = 5
+
+#images are named row1column1.png, row1column2.png, etc
+#this helps to set that up for image retrieval later
+def key(row, column):
+    return f"row{row}column{column}"
+
+#dictionary of descriptions for each scene to appear to the player in the default grid
 SCENE_TEXT = {
     (1, 1): "You are on a sandy beach. You see ocean to the south and a wall of rock to the west.",
     (1, 2): "You are on a sandy beach. You see a ship floating in the water to the south. a pirate calls out from the deck.",
@@ -20,3 +32,27 @@ SCENE_TEXT = {
     (4, 4): "You arrive at a small clearing in the forest next to a still lake. You see something glinting at the bottom of the lake. You see a wall of rock to the north.",
     (4, 5): "You are in a dense forest. You see walls of rock to the north and east."
 }
+
+#builds the grid locations based 
+def build_grid_locations():
+    locations = {}
+    for row in range(1, GRID_ROWS + 1):
+        for column in range(1, GRID_COLUMNS + 1):
+            north = key(row + 1, column) if row < GRID_ROWS else None
+            south = key(row - 1, column) if row > 1 else None
+            east = key(row, column + 1) if column < GRID_COLUMNS else None
+            west = key(row, column - 1) if column > 1 else None
+
+            locations[key(row, column)] = Location(
+                name=key(row, column),
+                story=SCENE_TEXT[(row, column)],
+                north=north, south=south, east=east, west=west
+            )
+    #row 4 column 1 north exit is the locked door. it has two images
+    door = locations[key(4, 1)]
+    door.north = "row5column1"
+    door.locked = True
+    door.locked_message = "A massive locked door blocks the way north."
+    door.image = "assets/images/row4column1closed.png"
+
+    return locations
