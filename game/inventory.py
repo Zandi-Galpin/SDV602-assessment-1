a02597dbe569b8d93b9478b712de60731cab52f4
Zandi Galpin <zandi.galpin@live.nmit.ac.nz>
2026-09-07 19:45:17 +1200

    updated inventory class with functions for finding, having, adding, removing items, adding and spending gold. added equip function which alters the players attack and block. added using function (for consumables) which increases the players hp but not above the max. need to update handle_equip and handle_use in game.py next, then update headless testing

--- game/inventory.py
+++ game/inventory.py
@@ -1,17 +1,77 @@
 class Inventory:
+    """Tracks the player's items and gold.
+
+    Items are looked up by name (not case sensitive, because command parser
+     makes the inputs lowercase before it gets to this).
+    """
+
     def __init__(self):
         self.items = []
+        self.gold = 0
 
-    def has_item(self, item):
-        return item in self.items
+    def find_item(self, item_name):
+        for item in self.items:
+            if item.name == item_name:
+                return item
+        return None
+
+    def has_item(self, item_name):
+        return self.find_item(item_name) is not None
 
     def add_item(self, item):
-        if not self.has_item(item):
-            self.items.append(item)
-            return True
+        self.items.append(item)
+        return True
 
+    def remove_item(self, item_name):
+        item = self.find_item(item_name)
+        if item:
+            self.items.remove(item)
+            return True
         return False
 
-    def use_item(self, item, player):
-        print('Not Implemented')
-        pass
+    def add_gold(self, amount):
+        self.gold += amount
+
+    def spend_gold(self, amount):
+        if self.gold >= amount:
+            self.gold -= amount
+            return True
+        return False
+
+    def equip_item(self, item_name, player):
+        #equiping adjusts the users damage/block.
+        item = self.find_item(item_name)
+        if not item:
+            return f"You don't have {item_name}."
+
+        if item.equipped:
+            item.equipped = False
+            player.damage -= item.damage
+            player.block -= item.block
+            return f"You unequipped {item.name}, losing " + \
+                f"{item.damage} attack damage and {item.block} block"
+
+        item.equipped = True
+        player.damage += item.damage
+        player.block += item.block
+        return f"You equipped {item.name}, gaining " + \
+                f"{item.damage} attack damage and {item.block} block"
+
+    def use_item(self, item_name, player):
+        #Uses a consumable (healing)
+        item = self.find_item(item_name)
+        if not item:
+            return f"You don't have {item_name}."
+
+        if not item.heal:
+            return f"{item.name} can't be used."
+
+        if item.used:
+            return f"{item.name} has already been used."
+
+        item.used = True
+        healed = min(item.heal, player.health_max - player.health_current)
+        player.health_current += healed
+        self.remove_item(item_name)
+
+        return f"You used {item.name} and healed {healed} HP."
